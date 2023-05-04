eb5798da443bdf641f162e4db2d784363a052855
pedro <104280113+pedrrr@users.noreply.github.com>
2023-05-04 15:52:14 -0300

    Delete projeto palmas directory

--- projeto palmas/data/database.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-const pool = mysql.createPool({
-    host: 'localhost',
-    database: 'odpalmas',
-    user: 'odPalmas',
-    password: 'odPalmas'
-});
-
-module.exports = pool;
